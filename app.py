4924555eaf99b4fd5369641f0f0fda9fa3b78944
jplepinsk1 <jplepinsk@gmail.com>
2025-05-21 09:05:24 -0300

    Novos links

--- app.py
+++ app.py
@@ -1,78 +1,12 @@
 from flask import Flask, render_template
+from cards_data import cards
 
-alunos = [
-    {'nome':'Ana Beatriz Magalhães Bicudo',
-     'cards':'https://anabeatriz-mb.github.io/automatic-adventure/'},
-    {'nome':'Ana Clara Mendes Proença',
-      'cards':'https://anaclaramendes399.github.io/Projeto-Certo'},
-    {'nome':'Beatriz Sousa de Andrade',
-      'cards':'https://beatriz-sousa-andrade.github.io/filmes-de-terror-classicos'},
-    {'nome':'Daniel Pupo de Morais Santos',
-     'cards':'https://danielpupo.github.io/Cards-Futsal'},
-    {'nome':'Emannoel Henrique Soares de Oliveira',
-     'cards':'https://emannoel-henrique.github.io/Projetos-DS'},
-    {'nome':'Felipe Barros Souza',
-     'cards':'https://barros-sz.github.io/Cards-Sobre-V-F'},
-    {'nome':'Felix Fonseca Nogueira',
-     'cards':'https://fonseca-felix.github.io/felix.card'},
-    {'nome':'Guilherme da Costa Silva',
-     'cards':'https://glcost.github.io/Trabalho-de-Cards'},
-    {'nome':'Isabelly Ferreira de Lima',
-     'cards':'https://isabelly-ferreira.github.io/Projeto-Cards/'},
-    {'nome':'Isadora Jucá de Lima',
-     'cards':'https://isajuca.github.io/card-dogs'},
-    {'nome':'João Guilherme Barros Arruda',
-     'cards':'https://jgarrudaa.github.io/Card-de-Filmes/'},
-    {'nome':'João Pedro Stadler Roza Santos',
-     'cards':'https://jpsantosx.github.io/card-de-jogos'},
-    {'nome':'João Victor Marcondes M. Barbosa de Queiroz',
-     'cards':'https://jvictormarcon.github.io/Cards/'},
-    {'nome':'Joaquim Antonio dos Santos Neto',
-     'cards':'https://joaquimneto17.github.io/Elenco-Santos'},
-    {'nome':'John Cristopher de Sousa',
-     'cards':'https://john-cristopher.github.io/card_carros'},
-    {'nome':'Julio Aurélio Souza',
-     'cards':'https://julio-aurelio.github.io/SENAI'},
-    {'nome':'Livia Domingues Matos',
-     'cards':'https://liviadmatos.github.io/upgraded-telegram'},
-    {'nome':'Lorena Rinaldo Moreira',
-     'cards':'https://lorena-rinaldo.github.io/cards-pokemon/'},
-    {'nome':'Lucas Assis Andrela',
-     'cards':'https://lucasandrela.github.io/Cards'},
-    {'nome':'Luiz Felipe de Lima Barros',
-     'cards':'https://luizfelipebarros.github.io/Project-cards'},
-    {'nome':'Maria Júllia Schimidt Ott Prestes',
-     'cards':'https://mariajschimidt.github.io/flores-tipos'},
-    {'nome':'Mauro José da Silva Junior',
-     'cards':'https://omaurojunior.github.io/Estruturando-site'},
-    {'nome':'Mayara Alves de Oliveira',
-     'cards':'https://mayaa-lves.github.io/books.cards.b'},
-    {'nome':'Nicolly Oliveira dos Santos',
-     'cards':'https://nicollyoliveiras.github.io/Cards.oth'},
-    {'nome':'Pablo Gomes da Silva',
-     'cards':'https://pablo-gomes.github.io/Cards-carros-de-luxo'},
-    {'nome':'Pedro Henrique Francisconi Ferreira',
-     'cards':'https://pedrofrancisconi.github.io/PokeNBa'},
-    {'nome':'Pedro Henrique Vasconcelos Matilde',
-     'cards':'https://pedrohenriquevm.github.io/Projetos'},
-    {'nome':'Pedro Vitor Barros de Morais',
-     'cards':'https://pedromorais15.github.io/pedro-page'},
-    {'nome':'Pietro Rodrigues Torresani Mantuan',
-     'cards':'https://mantuanpietro.github.io/CARDS'},
-    {'nome':'Rebeca Diniz Gonçalves',
-     'cards':'https://rebecadiniz.github.io/estudo-da-filosofia/'},
-    {'nome':'Vinicius Mariozi Oliveira',
-     'cards':'https://viniciusmariozioliveira.github.io/cards'},
-    {'nome':'Yan Matheus Proença Camargo',
-     'cards':'https://yan-proenca.github.io/card-sombras-cicatrizes'}
-    
-]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', alunos=alunos)
+    return render_template('index.html', cards=cards)
 
 
 if __name__ == '__main__':
---
+++ cards_data.py
@@ -0,0 +1,126 @@
+cards = [
+    {'nome':'Ana Beatriz Magalhães Bicudo',
+     'link1':'https://anabeatriz-mb.github.io/automatic-adventure/',
+     'link2':'https://anabeatriz-mb.github.io/pagina-de-noticia/'},
+
+    {'nome':'Ana Clara Mendes Proença',
+      'link1':'https://anaclaramendes399.github.io/Projeto-Certo',
+      'link2':'https://anaclaramendes399.github.io/jornal/'},
+
+    {'nome':'Beatriz Sousa de Andrade',
+      'link1':'https://beatriz-sousa-andrade.github.io/filmes-de-terror-classicos',
+      'link2':'https://beatriz-sousa-andrade.github.io/Jornal/'},
+
+    {'nome':'Daniel Pupo de Morais Santos',
+     'link1':'https://danielpupo.github.io/Cards-Futsal'},
+
+    {'nome':'Emannoel Henrique Soares de Oliveira',
+     'link1':'https://emannoel-henrique.github.io/Projetos-DS',
+     'link2':'https://emannoel-henrique.github.io/GAMENEWS/'},
+
+    {'nome':'Felipe Barros Souza',
+     'link1':'https://barros-sz.github.io/Cards-Sobre-V-F',
+     'link2':'https://barros-sz.github.io/Jornal-do-Barros/'},
+
+    {'nome':'Felix Fonseca Nogueira',
+     'link1':'https://fonseca-felix.github.io/felix.card',
+     'link2':'https://fonseca-felix.github.io/wireframe'},
+
+    {'nome':'Guilherme da Costa Silva',
+     'link1':'https://glcost.github.io/Trabalho-de-Cards',
+     'link2':'https://glcost.github.io/noticias2'},
+
+    {'nome':'Isabelly Ferreira de Lima',
+     'link1':'https://isabelly-ferreira.github.io/Projeto-Cards/',
+     'link2':'https://isabelly-ferreira.github.io/Pagina-de-Noticia'},
+
+    {'nome':'Isadora Jucá de Lima',
+     'link1':'https://isajuca.github.io/card-dogs',
+     'link2':'https://isajuca.github.io/noticias_economia'},
+
+    {'nome':'João Guilherme Barros Arruda',
+     'link1':'https://jgarrudaa.github.io/Card-de-Filmes/',
+     'link2':'https://jgarrudaa.github.io/jornal-cinenews'},
+
+    {'nome':'João Pedro Stadler Roza Santos',
+     'link1':'https://jpsantosx.github.io/card-de-jogos',
+     'link2':'https://jpsantosx.github.io/sitedenoticias'},
+
+    {'nome':'João Victor Marcondes M. Barbosa de Queiroz',
+     'link1':'https://jvictormarcon.github.io/Cards/',
+     'link2':'https://jvictormarcon.github.io/POP-NEWS/'},
+
+    {'nome':'Joaquim Antonio dos Santos Neto',
+     'link1':'https://joaquimneto17.github.io/Elenco-Santos',
+     'link2':'https://joaquimneto17.github.io/NetGol/'},
+
+    {'nome':'John Cristopher de Sousa',
+     'link1':'https://john-cristopher.github.io/card_carros',
+     'link2':'https://john-cristopher.github.io/Jornal-MotorSport'},
+
+    {'nome':'Julio Aurélio Souza',
+     'link1':'https://julio-aurelio.github.io/SENAI',
+     'link2':'https://julio-aurelio.github.io/projeto_jornal-main'},
+
+    {'nome':'Livia Domingues Matos',
+     'link1':'https://liviadmatos.github.io/upgraded-telegram',
+     'link2': 'https://liviadmatos.github.io/noticias'},
+
+    {'nome':'Lorena Rinaldo Moreira',
+     'link1':'https://lorena-rinaldo.github.io/link1-pokemon/',
+     'link2':'https://lorena-rinaldo.github.io/GNN-News'},
+
+    {'nome':'Lucas Assis Andrela',
+     'link1':'https://lucasandrela.github.io/Cards',
+     'link2':'https://lucasandrela.github.io/douglas'},
+
+    {'nome':'Luiz Felipe de Lima Barros',
+     'link1':'https://luizfelipebarros.github.io/Project-link1',
+     'link2':'https://luizfelipebarros.github.io/site-de-noticias'},
+
+    {'nome':'Maria Júllia Schimidt Ott Prestes',
+     'link1':'https://mariajschimidt.github.io/flores-tipos',
+     'link2':'https://mariajschimidt.github.io/wireframe-mj'},
+
+    {'nome':'Mauro José da Silva Junior',
+     'link1':'https://omaurojunior.github.io/Estruturando-site',
+     'link2':'https://omaurojunior.github.io/jornal-mauro'},
+
+    {'nome':'Mayara Alves de Oliveira',
+     'link1':'https://mayaa-lves.github.io/books.link1.b',
+     'link2':'https://mayaa-lves.github.io/GOLDEN-30-nba'},
+
+    {'nome':'Nicolly Oliveira dos Santos',
+     'link1':'https://nicollyoliveiras.github.io/Cards.oth',
+     'link2':'https://nicollyoliveiras.github.io/jornalEcoNewss'},
+
+    {'nome':'Pablo Gomes da Silva',
+     'link1':'https://pablo-gomes.github.io/Cards-carros-de-luxo',
+     'link2':'https://pablo-gomes.github.io/site-noticia/'},
+
+    {'nome':'Pedro Henrique Francisconi Ferreira',
+     'link1':'https://pedrofrancisconi.github.io/PokeNBa'},
+
+    {'nome':'Pedro Henrique Vasconcelos Matilde',
+     'link1':'https://pedrohenriquevm.github.io/Projetos',
+     'link2':'https://pedrohenriquevm.github.io/Noticias-Camp.-Brasileiro/'},
+
+    {'nome':'Pedro Vitor Barros de Morais',
+     'link1':'https://pedromorais15.github.io/pedro-page'},
+
+    {'nome':'Pietro Rodrigues Torresani Mantuan',
+     'link1':'https://mantuanpietro.github.io/CARDS',
+     'link2':'https://mantuanpietro.github.io/Kings-league-SENAI'},
+
+    {'nome':'Rebeca Diniz Gonçalves',
+     'link1':'https://rebecadiniz.github.io/estudo-da-filosofia/',
+     'link2':'https://rebecadiniz.github.io/ECONOMIA-BRASIL/'},
+
+    {'nome':'Vinicius Mariozi Oliveira',
+     'link1':'https://viniciusmariozioliveira.github.io/link1',
+     'link2':'https://viniciusmariozioliveira.github.io/Teste-Jornal/'},
+
+    {'nome':'Yan Matheus Proença Camargo',
+     'link1':'https://yan-proenca.github.io/card-sombras-cicatrizes',
+     'link2':'https://yan-proenca.github.io/site-about-tech/'}    
+]
